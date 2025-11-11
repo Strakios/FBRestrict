fb04ac4a3565ad0943a7941d82e8bec553b8ffa3
Strakios <58398269+Strakios@users.noreply.github.com>
2025-11-10 21:29:05 -0500

    Create FBRestrict.meta.js

---
+++ FBRestrict.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Facebook Restricción
+// @namespace    https://github.com/Strakios/FBRestrict
+// @version      2.0
+// @description  Detecta imágenes relevantes
+// @author       Strakios
+// @match        https://www.facebook.com/*
+// @icon         https://www.facebook.com/favicon.ico
+// @updateURL    https://raw.githubusercontent.com/Strakios/FBRestrict/main/FBRestrict.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Strakios/FBRestrict/main/FBRestrict.user.js
+// @grant        none
+// ==/UserScript==
